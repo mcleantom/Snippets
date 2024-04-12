fe38170eb156e053aaea2448a11344ccd863bf9b
Tom McLean <64887735+mcleantom@users.noreply.github.com>
2024-04-12 11:24:59 +0100

    Update worker.py

--- Networking/RouterDealer/worker.py
+++ Networking/RouterDealer/worker.py
@@ -1,30 +1,14 @@
 import zmq
-import time
-from uuid import uuid4
 
 context = zmq.Context()
-client = context.socket(zmq.DEALER)
-identity = str(uuid4())
-client.identity = identity.encode("ascii")
-client.connect("tcp://localhost:5560")
+worker = context.socket(zmq.DEALER)
+worker.connect("tcp://localhost:5561")
 
-poller = zmq.Poller()
-poller.register(client, zmq.POLLIN)
-
-for request_num in range(10):
-    print("Sending request %s to broker" % request_num)
-    client.send_multipart([b'Hello', b'World'])
-
-    socks = dict(poller.poll(5000))  # Timeout set to 5000 milliseconds (5 seconds)
-
-    if client in socks:
-        message = client.recv()
-        print("Received reply %s [ %s ]" % (request_num, message))
-    else:
-        print("No response from broker for request %s" % request_num)
-
-    time.sleep(1)
-
-poller.unregister(client)
-client.close()
-context.term()
+while True:
+    message = worker.recv_multipart()
+    print("Received message from broker:", message)
+    # Process the message here
+    # ...
+    # Respond back to the broker
+    response = b"Processed: " + message[1]
+    worker.send_multipart([message[0], response])
